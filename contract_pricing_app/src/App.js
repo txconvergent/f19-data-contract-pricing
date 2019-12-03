7466c1cb69ff1753bc35c7fe3c602140c7510e83
Brayden Miller <braydenmiller33@gmail.com>
2019-12-02 19:08:15 -0600

    updated homepage

--- contract_pricing_app/src/App.js
+++ contract_pricing_app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './assets/css/main.css'
+import './assets/css/main.css';
+import image from './assets/css/images/SportuneOrange.png';
 
 function App() {
   return (
@@ -12,17 +13,24 @@ function App() {
 
               <div class="inner">
                 <header>
-                  <h1><a href="left-sidebar.html" id="logo">Sportune</a></h1>
-                  <p><b>insert slogan here</b></p>
+
+
+
+                <div className="logoimagebox">
+                  <img className="logoimage" src={image} alt="Sportune">
+                  </img>
+                </div>
+
+
+
                 </header>
               </div>
 
 
               <nav id="nav">
                 <ul>
-                  <li><a href="index.html">Home</a></li>
                   <li>
-                    <a href="#">Dropdown</a>
+                    <a href="#">Home</a>
                     <ul>
                       <li><a href="#">Lorem ipsum dolor</a></li>
                       <li><a href="#">Magna phasellus</a></li>
@@ -39,105 +47,30 @@ function App() {
                       <li><a href="#">Veroeros feugiat</a></li>
                     </ul>
                   </li>
-                  <li><a href="left-sidebar.html">Left Sidebar</a></li>
-                  <li><a href="right-sidebar.html">Right Sidebar</a></li>
-                  <li><a href="no-sidebar.html">No Sidebar</a></li>
+                  <li><a href="left-sidebar.html">Player Prediction</a></li>
+                  <li><a href="right-sidebar.html">Personal Prediction</a></li>
                 </ul>
               </nav>
 
           </div>
 
-          <section id="banner">
-            <header>
-              <h2>Hi. You're looking at <strong>Helios</strong>.</h2>
-              <p>
-                A (free) responsive site template by <a href="http://html5up.net">HTML5 UP</a>.
-                Built with HTML5/CSS3 and released under the <a href="http://html5up.net/license">CCA</a> license.
-              </p>
-            </header>
-          </section>
-
           <section class="carousel">
             <div class="reel">
 
               <article>
-                <a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Pulvinar sagittis congue</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
                 <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
                 <header>
-                  <h3><a href="#">Fermentum sagittis proin</a></h3>
+                  <h3><a href="#">Player Prediction</a></h3>
                 </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                <p>Get data-driven insights on player salaries. Input a player name and see how much the Sportune model predicts they should get paid!</p>
               </article>
 
               <article>
                 <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
                 <header>
-                  <h3><a href="#">Sed quis rhoncus placerat</a></h3>
+                  <h3><a href="#">Personal Prediction</a></h3>
                 </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
-                <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Ultrices urna sit lobortis</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
-                <a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Varius magnis sollicitudin</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
-                <a href="#" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Pulvinar sagittis congue</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
-                <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Fermentum sagittis proin</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
-                <a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Sed quis rhoncus placerat</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
-                <a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Ultrices urna sit lobortis</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
-              </article>
-
-              <article>
-                <a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
-                <header>
-                  <h3><a href="#">Varius magnis sollicitudin</a></h3>
-                </header>
-                <p>Commodo id natoque malesuada sollicitudin elit suscipit magna.</p>
+                <p>Be an NBA player for a day. Input your own statistics and see how much the Sportune model predicts you should get paid!</p>
               </article>
 
             </div>
