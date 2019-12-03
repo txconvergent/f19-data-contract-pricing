3d5550942ed70875eccadeccfed1bfcfe65e5814
Brayden Miller <braydenmiller33@gmail.com>
2019-12-02 19:45:46 -0600

    more front-end dev

--- contract_pricing_app/src/App.js
+++ contract_pricing_app/src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './assets/css/main.css';
-import image from './assets/css/images/SportuneOrange.png';
+import image from './assets/css/images/resizedfinallogo.png';
 
 function App() {
   return (
     <div className="App">
       <div id="page-wrapper">
 
-          <div id="header">
 
               <div class="inner">
                 <header>
@@ -25,42 +24,15 @@ function App() {
 
                 </header>
               </div>
-
-
-              <nav id="nav">
-                <ul>
-                  <li>
-                    <a href="#">Home</a>
-                    <ul>
-                      <li><a href="#">Lorem ipsum dolor</a></li>
-                      <li><a href="#">Magna phasellus</a></li>
-                      <li><a href="#">Etiam dolore nisl</a></li>
-                      <li>
-                        <a href="#">And a submenu &hellip;</a>
-                        <ul>
-                          <li><a href="#">Lorem ipsum dolor</a></li>
-                          <li><a href="#">Phasellus consequat</a></li>
-                          <li><a href="#">Magna phasellus</a></li>
-                          <li><a href="#">Etiam dolore nisl</a></li>
-                        </ul>
-                      </li>
-                      <li><a href="#">Veroeros feugiat</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="left-sidebar.html">Player Prediction</a></li>
-                  <li><a href="right-sidebar.html">Personal Prediction</a></li>
-                </ul>
-              </nav>
-
-          </div>
+              </div>
 
           <section class="carousel">
             <div class="reel">
 
               <article>
-                <a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+                <a href="player_prediction.html" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
                 <header>
-                  <h3><a href="#">Player Prediction</a></h3>
+                  <h3><a href="player_prediction.html">Player Prediction</a></h3>
                 </header>
                 <p>Get data-driven insights on player salaries. Input a player name and see how much the Sportune model predicts they should get paid!</p>
               </article>
@@ -77,7 +49,6 @@ function App() {
           </section>
 
               </div>
-            </div>
   );
 }
 
